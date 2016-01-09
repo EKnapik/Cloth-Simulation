e799d78b8ddb219526d9c7cf11e27c2271b750da
Eric Knapik <eric11mk@gmail.com>
2016-01-09 14:09:23 -0500

    Initial Commit

---
+++ ClothSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClothSimulation
+//
+//  Created by Eric Knapik on 1/9/16.
+//  Copyright © 2016 EKnapik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
